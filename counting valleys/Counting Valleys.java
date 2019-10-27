fb9920f65b60fbe61a255c739a587131c81488ec
Nanjesh Ramesh <nanjeshramesh7@gmail.com>
2019-10-26 17:03:07 -0700

    Add files via upload

---
+++ counting valleys/Counting Valleys.java
@@ -0,0 +1,30 @@
+﻿import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int level = 1;
+        int valleys = 0;
+
+        String str = in.next();
+        for(int i = 0; i < n; i++) {
+            char c = str.charAt(i);
+            if(c == 'U'){
+                if(level < 1 && (level+1) == 1) {
+                    valleys++;
+                }
+                level++;
+            }else{
+                level--;
+            }
+
+        }
+        System.out.println(valleys);
+    }
+}
